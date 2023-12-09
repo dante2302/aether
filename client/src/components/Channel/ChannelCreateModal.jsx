9d17f26cc78fc8ec0545ed878ffca94b097d9fed
dante <guchoo923@gmail.com>
2023-12-09 02:42:00 +0200

    Style Channel Creation Form

--- client/src/components/Channel/ChannelCreateModal.jsx
+++ client/src/components/Channel/ChannelCreateModal.jsx
@@ -1,16 +1,12 @@
 import ModalPrototype from "../ModalPrototype.jsx"
 import ChannelCreateForm from './ChannelCreateForm.jsx'
-import styles from './styles/ChannelCreateModal.module.css'
+
 
 const ChannelCreateModal = ({toggleChannelModal}) => {
 
   return (
     <ModalPrototype toggleModal={toggleChannelModal}>
-        <div className={styles['content']}>
-          <h3>Name</h3>
-          <h6>Community names including capitalization cannot be changed.</h6>
-          <ChannelCreateForm toggleChannelModal={toggleChannelModal} />
-        </div>
+    <ChannelCreateForm toggleChannelModal={toggleChannelModal} />
     </ModalPrototype>
   )
 }
